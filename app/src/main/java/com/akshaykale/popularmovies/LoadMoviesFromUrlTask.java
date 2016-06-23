f87e316fdf7af532aa474daf156a7c127676495b
akshaykale <ark.akshaykale@gmail.com>
2016-06-23 09:42:38 +0530

    > Async Task to load data from url / HttpURLconnection code added

--- app/src/main/java/com/akshaykale/popularmovies/LoadMoviesFromUrlTask.java
+++ app/src/main/java/com/akshaykale/popularmovies/LoadMoviesFromUrlTask.java
@@ -3,6 +3,14 @@ package com.akshaykale.popularmovies;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by Akshay on 6/23/2016.
  */
@@ -12,6 +20,8 @@ public class LoadMoviesFromUrlTask extends AsyncTask<String,String,String>{
     Context mContext;
     String mUrl;
 
+    private final String USER_AGENT = "Android";
+
     public LoadMoviesFromUrlTask(Context mContext, String mUrl) {
         this.mContext = mContext;
         this.mUrl = mUrl;
@@ -19,6 +29,39 @@ public class LoadMoviesFromUrlTask extends AsyncTask<String,String,String>{
 
     @Override
     protected String doInBackground(String... params) {
+        try {
+            URL obj = new URL(params[0]);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            //add request header
+            /**
+             Content-Type:application/json
+             trakt-api-version:2
+             trakt-api-key:[client_id]
+             * */
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("trakt-api-version", "2");
+            con.setRequestProperty("trakt-api-key", Constants.CLIENT_ID);
+
+            int responseCode = con.getResponseCode();
+
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
